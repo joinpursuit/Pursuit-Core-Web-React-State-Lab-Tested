298454c9d73559a9f84eee7046e9eb2e57d55c5c
Shoaib Dar <SDAR30@gmail.com>
2021-04-10 17:00:53 -0400

    added all elements

--- src/App.js
+++ src/App.js
@@ -3,8 +3,57 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
+  state = { score: 0, incrementValue: 1 }
+
+
+  
+
+  increment=()=> {
+    if(this.state.score >= 10){
+      const h2 = document.querySelector(".win")
+      const play = document.querySelector(".play")
+      const button = document.querySelector(".button")
+      const button2 = document.querySelector(".button2")
+      button.style = "opacity: 0"
+      button2.style = "opacity: 0"
+      h2.textContent = "You Win!"
+      play.textContent= "Play Again?";
+      debugger
+    }
+    this.setState({ score: this.state.score + this.state.incrementValue })
+  }
+
+    
+    
+
+  increaseValue = () =>{
+    const {score, incrementValue} = this.state;
+    if(score > 10){
+      this.setState({
+        score: score - 10, incrementValue: incrementValue + 1
+      })
+    }else{
+      alert("You can't afford that!")
+    }
+  }
+
+  resetGame = () =>{
+    const {score, incrementValue} = this.state;
+    this.setState({
+      score: 0, incrementValue: 1
+    })
+  }
+
+
   render() {
-    return "Hello, world!";
+    const { score, incrementValue } = this.state;
+    return (<>
+      <h1>Current Score: {score}</h1>
+      <button className="button" onClick={this.increment}>+{incrementValue}</button>
+      <button className="button2" onClick={this.increaseValue}>Pay 10 points to change from +{incrementValue} to +{incrementValue+1}</button>
+      <h2 className="win"></h2>
+      <button className="play" onClick={this.resetGame}></button>
+    </>)
   }
 }
 
