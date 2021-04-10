03f2c2d48c3975ba86f6788cdadf1e7cc8d3d419
JDJ97 <jdj97@techie.com>
2021-04-10 17:36:39 -0400

    Finished assignment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
-
+import Clicker from "./Clicker"
 import "./App.css";
 
 class App extends React.Component {
   render() {
-    return "Hello, world!";
+    return (
+      <div>
+        <Clicker />
+      </div>
+    );
   }
 }
 
