f5b58c12bb08bd0649a5751285be3a6aadcc2761
Shoaib Dar <SDAR30@gmail.com>
2021-04-10 18:14:18 -0400

    finished styling

--- src/App.js
+++ src/App.js
@@ -5,24 +5,14 @@ import "./App.css";
 class App extends React.Component {
   state = { score: 0, incrementValue: 1 }
 
-
-
-
   increment = () => {
     const { score, incrementValue } = this.state;
     this.setState({ score: score + incrementValue })
-    if (score >= 100) {
-      debugger
-    }
-
   }
-
-
-
 
   increaseValue = () => {
     const { score, incrementValue } = this.state;
-    if (score > 9) {
+    if (score >= 10) {
       this.setState({
         score: score - 10, incrementValue: incrementValue + 1
       })
@@ -33,30 +23,26 @@ class App extends React.Component {
 
   resetGame = () => {
     const { score, incrementValue } = this.state;
-    this.setState({
-      score: 0, incrementValue: 1
-    })
+    this.setState({ score: 0, incrementValue: 1 })
   }
-
 
   render() {
     const { score, incrementValue } = this.state;
     if (score < 100) {
       return (<>
         <h1>Current Score: {score}</h1>
-        <button className="button" onClick={this.increment}>+{incrementValue}</button>
-        <button className="button2" onClick={this.increaseValue}>Pay 10 points to change from +{incrementValue} to +{incrementValue + 1}</button>
+        <button onClick={this.increment}>+{incrementValue}</button>
+        <br></br>
+        <br></br>
+        <button onClick={this.increaseValue}>Pay 10 points to change from +{incrementValue} to +{incrementValue + 1}</button>
       </>)
     } else {
       return (<>
        <h1>Current Score: {score}</h1>
-        <h2 className="win">You Win!</h2>
-        <button className="play" onClick={this.resetGame}>Play again?</button>
+        <h2>You Win!</h2>
+        <button onClick={this.resetGame}>Play again?</button>
       </>)
     }
-
-
   }
 }
-
 export default App;
