b41f25ef76bd7396acd924253a88d9348f932afb
Coreen Cooper <coreencooper@pursuit.org>
2021-04-10 17:42:38 -0400

    fix buttons

--- src/Components/GameComponent.js
+++ src/Components/GameComponent.js
@@ -3,21 +3,27 @@ import React from "react";
 class GameComponent extends React.Component {
   constructor(props) {
     super();
-    this.state = { count: 0 , incrementAmount:1};
+    this.state = { score: 0 , incrementAmount: 1,  newAmount: 2};
   }
   increment = () => {
-    this.setState({ count: this.state.count + this.state.incrementAmount });
+    this.setState({ score: this.state.score + this.state.incrementAmount });
   };
 
   incrementButton = () =>{
-      this.setState({ incrementAmount: this.state.incrementAmount +1 })
+    if (this.state.score < 10) {
+      alert("You can't afford that!");
+    } else {
+      this.setState({ incrementAmount: this.state.incrementAmount +1 });
+      this.setState({ newAmount: this.state.newAmount +1 });
+      this.setState({ score: this.state.score - 10 });
+    }
   }
   render() {
     return (
       <div>
-        <h2>Current Score: {this.state.count}</h2>
+        <h2>Current Score: {this.state.score}</h2>
         <button onClick={this.increment}>+{this.state.incrementAmount}</button>
-        <button onClick={this.incrementButton}> increment</button>
+        <button onClick={this.incrementButton}>Pay 10 points to change from +{this.state.incrementAmount} to +{this.state.newAmount}</button>
       </div>
     );
   }
