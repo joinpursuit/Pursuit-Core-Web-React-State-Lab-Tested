91c3e7c68ba94a79b8f82a8ce9c57db085e28b2f
Cari <t.carina@rocketmail.com>
2021-04-11 16:09:52 -0400

    merging

--- src/components/score.js
+++ src/components/score.js
@@ -1,33 +1,36 @@
-import React from "react"
+import React from "react";
 
 class Score extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {score: 0, increment: 1}
-    }
+  constructor(props) {
+    super(props);
+    this.state = { score: 0, increment: 1 };
+  }
 
-    plus = () => {
-        this.setState({score: this.state.score + this.state.increment})
-    }
+  plus = () => {
+    this.setState({ score: this.state.score + this.state.increment });
+  };
 
-    increaseScore = () => {
-        if( this.state.score >= 10 ) {
-            this.setState({ score: this.state.score - 10 })
-            this.setState({ increment: this.state.increment + 1})
-        } else {
-            window.alert("You can't afford that!")
-        }
+  increaseScore = () => {
+    if (this.state.score >= 10) {
+      this.setState({ score: this.state.score - 10 });
+      this.setState({ increment: this.state.increment + 1 });
+    } else {
+      window.alert("You can't afford that!");
     }
+  };
 
-    render() {
-        return (
-            <div>
-                <button onClick={this.plus}> +{this.state.increment}</button>
-              Current Score:  { this.state.score } 
-             <button onClick={this.increaseScore}>Pay 10 points to change from +{this.state.increment} to +{this.state.increment + 1} </button>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1>Current Score: {this.state.score}</h1>
+        <button onClick={this.plus}> +{this.state.increment}</button>
+        <button onClick={this.increaseScore}>
+          Pay 10 points to change from +{this.state.increment} to +
+          {this.state.increment + 1}
+        </button>
+      </div>
+    );
+  }
 }
 
-export default Score
+export default Score;
