56ee370fa9aa76354aa6fa1a36089fa65afb0794
Flower Aviles <floweraviles@flowers-mini.myfiosgateway.com>
2021-04-11 09:41:50 -0400

    Solved it

--- src/Components/CurrentScore.js
+++ src/Components/CurrentScore.js
@@ -3,22 +3,28 @@ import React from "react"
 class CurrentScore extends React.Component {
     constructor (props) {
         super(props);
-        this.state = {currentscore: 0, buttonscore: +1}
+        this.state = {currentscore: 0, buttonscore: 1}
     }
 
     increment = () => {
         this.setState ((prevState)=>{
-            return {currentscore: prevState.currentscore +1, buttonscore: prevState.buttonscore +1}
-        })
+            return {currentscore: prevState.currentscore +1}
+        });
     }
 
+    incrementButton = () => {
+        this.setState ({buttonscore: this.state.buttonscore +1})
+    }
+    
+
     decrement = () => {
-        const { currentscore } = this.state;
+        let { currentscore } = this.state;
         if(currentscore  <10) {
             return alert ("You can't afford this")
         } else {
             this.setState({currentscore: currentscore - 10})
-            
+            let { buttonscore } = this.state;
+            this.setState({buttonscore: buttonscore + 1 })
         }
     }
 
@@ -28,9 +34,10 @@ class CurrentScore extends React.Component {
             <div>
                 <h2>Current Score : {currentscore}</h2>
                 {/* Clicks: {currentscore} */}
-                <button onClick={this.increment}>`{buttonscore}`</button>
+                <button onClick={this.increment}> +{this.state.buttonscore}</button>
+                {/* Clicks: {this.state.buttonscore} */}
                 <br></br>
-                <button onClick={this.decrement}> Pay 10 points to change from +1 to +2 </button>
+                <button  class={ this.state.buttonscore} onClick={this.decrement}> Pay 10 points to change from +{this.state.buttonscore} to +{this.state.buttonscore +1} </button>
             </div>
         )
     }
