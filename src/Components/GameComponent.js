4f427f876d00b8fa66ed232e1e3872a2aa7be201
Jonathan Bedon <bedonjonathan@gmail.com>
2021-04-11 15:57:12 -0400

    Reset Current Score

--- src/Components/GameComponent.js
+++ src/Components/GameComponent.js
@@ -3,27 +3,35 @@ import React from "react";
 class GameComponent extends React.Component {
   constructor(props) {
     super();
-    this.state = { score: 0 , incrementAmount: 1,  newAmount: 2};
+    this.state = { score: 0, incrementAmount: 1, newAmount: 2 };
   }
   increment = () => {
-    this.setState({ score: this.state.score + this.state.incrementAmount });
+    if (this.state.score >= 100) {
+      this.setState({ score: 0});
+    } else {
+
+      this.setState({ score: this.state.score + this.state.incrementAmount });
+    }
   };
 
-  incrementButton = () =>{
+  incrementButton = () => {
     if (this.state.score < 10) {
       alert("You can't afford that!");
     } else {
-      this.setState({ incrementAmount: this.state.incrementAmount +1 });
-      this.setState({ newAmount: this.state.newAmount +1 });
+      this.setState({ incrementAmount: this.state.incrementAmount + 1 });
+      this.setState({ newAmount: this.state.newAmount + 1 });
       this.setState({ score: this.state.score - 10 });
     }
-  }
+  };
   render() {
     return (
       <div>
         <h2>Current Score: {this.state.score}</h2>
         <button onClick={this.increment}>+{this.state.incrementAmount}</button>
-        <button onClick={this.incrementButton}>Pay 10 points to change from +{this.state.incrementAmount} to +{this.state.newAmount}</button>
+        <button onClick={this.incrementButton}>
+          Pay 10 points to change from +{this.state.incrementAmount} to +
+          {this.state.newAmount}
+        </button>
       </div>
     );
   }
