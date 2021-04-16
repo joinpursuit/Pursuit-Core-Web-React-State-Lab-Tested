1692c046d9e8dee6e32153f1df80b77a697fb2b5
cassidybeni <cassidybeni@pursuit.org>
2021-04-16 17:13:23 -0400

    Completed lab

--- src/App.js
+++ src/App.js
@@ -3,26 +3,53 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
-  constructor() {
+  constructor() { //defines state
     super()
 
     this.state = {
-      total: 0
+      total: 0,
+      value: 1
     }
   }
 
   handleClick = () =>{
-    this.setState({total: this.state.total +1})
+    this.setState({total: this.state.total + this.state.value})
+  }
+
+  handlePointsClick = () => {
+    if(this.state.total >= 10){
+      this.setState({
+        total: this.state.total - 10, 
+        value: this.state.value + 1
+      })
+    } else {
+      alert("You can't afford that!")
+    }
+  }
+
+  reset = () => {
+    this.setState({total: 0, value: 1})
   }
 
   render() {
-    return (
-      <div>
-        <h1>Current Score: {this.state.total}</h1>
-        <button onClick={this.handleClick}>+1</button>
-        <button>Pay 10 points to change from +1 to +2</button>
-      </div>
-    )
+    if(this.state.total < 100){
+      return (
+        <div>
+          <h1>Current Score: {this.state.total}</h1>
+          <button onClick={this.handleClick}>+{this.state.value}</button>
+          <button onClick={this.handlePointsClick}>Pay 10 points to change from +{this.state.value} to +{this.state.value +1}</button>
+        </div>
+      )
+    } else {
+      return (
+        <>
+          <h1>Current Score: {this.state.total}</h1>
+          <h2>You Win!</h2>
+          <button onClick={this.reset}>Play again?</button>
+        </>
+      )
+    }
+    
   }
 }
 
