a880930093d997cd6d6f50cafa6a0873754f6d27
Tamzeed Rahman <tamzeedrahman@gmail.com>
2021-04-17 03:42:59 -0400

    BRO REVIWEW THIS

--- src/App.js
+++ src/App.js
@@ -6,22 +6,58 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-    totalNumber : 0
+    totalNumber: 0,
+    incrementer: 1
     }
   }
 
   handleClick = () => {
-    this.setState({totalNumber: this.state.totalNumber + 5})
+    this.setState({
+      totalNumber: this.state.totalNumber + this.state.incrementer
+    })
   }
+
+  subtract = () => {
+    
+    if (this.state.totalNumber < 10){
+      window.alert("You can't afford that!")
+    } else {
+      this.setState({
+        totalNumber: this.state.totalNumber - 10 ,
+        incrementer: this.state.incrementer + 1
+      })
+    }
+  }
+
+  restart = () => {
+    this.setState({
+      totalNumber: 0 ,
+      incrementer: 1
+    })
+  }
+
+
+
+
+
   render() {
+    if (this.state.totalNumber < 100){
     return (
       <div>
         <h1>Current Score: {this.state.totalNumber}</h1>
-        <button onClick={this.handleClick}>+1</button>
-        <button>pay 10 points to increase from +1 to +2</button>
+        <button onClick={this.handleClick}>+{this.state.incrementer}</button>
+        <button onClick={this.subtract}>pay 10 points to increase from +{this.state.incrementer} to +{this.state.incrementer + 1}</button>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <h2>You Win!</h2>
+        <button onClick={this.restart}>Play again?</button>
       </div>
     )
   }
+}
 }
 
 export default App;
