5583e992e2a8b5f36f7ec093529e7b4e6884b630
Daliz Cruz <dalizcruz@gmail.com>
2021-04-16 21:39:25 -0400

    buttons added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ import "./App.css";
 
 class App extends React.Component {
   render() {
-    return "Hello, world!";
+    return (
+      
+      <div>
+        <h1>Current Score: {score}</h1>
+        <button onClick={}> +{value}</button>
+        <button onClick={}> Pay 10 points to change from +1 to +2</button>
+      {/* <Score/> */}
+      </div>
+    );
   }
 }
 
