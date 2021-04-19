7e89ff3c9ef188e178dab4cebaa4a9a1e8b075ba
EsayH <ez@EsayHs-Mac-mini.local>
2021-04-18 23:53:56 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,8 +3,26 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      totalNum: 0
+      
+    }
+  }
+
+  handleClick = () => {
+    this.setState({ totalNum : this.state.totalNum + 1 })
+  }
   render() {
-    return "Hello, world!";
+    return (
+      <div>
+        <h2>Current Score: {this.state.totalNum}</h2>
+        <button onClick={this.handleClick}>+1</button>
+        <button>Pay 10 points to change from +1 to +2</button>
+      </div>
+    )
   }
 }
 
