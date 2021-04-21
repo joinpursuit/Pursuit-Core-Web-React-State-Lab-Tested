fc6c56e1536786049b7afa010c1fa5bf2c625c24
Kelvin Zheng <kzheng000@citymail.cuny.edu>
2021-04-20 23:37:23 -0400

    test passed

--- src/App.js
+++ src/App.js
@@ -3,8 +3,58 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      currentValue: 0,
+      addedValue: 1
+    }
+  }
+  buttonClick = () => {
+    this.setState({
+      currentValue: this.state.currentValue + this.state.addedValue
+    })
+  }
+  pointChangeClick = () => {
+    if (this.state.currentValue >= 10) {
+      this.setState({
+        currentValue: this.state.currentValue - 10,
+        addedValue: this.state.addedValue + 1
+      })
+    }
+    else {
+      alert(`You can't afford that!`)
+    }
+  }
+  restart = () => {
+    this.setState({
+      currentValue: 0,
+      addedValue: 1
+    })
+  }
   render() {
-    return "Hello, world!";
+    if(this.state.currentValue > 100) {
+      return(
+      <>
+        <h1>Current Score: {this.state.currentValue}</h1>
+
+        <h2>You Win!</h2>
+
+        <button onClick={this.restart}>Play again?</button>
+      </>
+      )
+    }
+    else {
+      return(
+      <>
+        <h1>Current Score: {this.state.currentValue}</h1>
+
+        <button onClick={this.buttonClick}>+{this.state.addedValue}</button>
+
+        <button onClick={this.pointChangeClick}>Pay 10 points to change from +{this.state.addedValue} to +{(this.state.value + 1)}</button>
+      </>
+      )
+    }
   }
 }
 
