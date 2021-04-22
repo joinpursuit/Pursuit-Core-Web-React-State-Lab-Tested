b3502d75464c0097a1bc1dbb352666a4ac43c5b5
EsayH <ez@EsayHs-Mac-mini.local>
2021-04-22 00:54:22 -0400

    State Lab

--- src/App.js
+++ src/App.js
@@ -1,29 +1,60 @@
-import React from "react";
+import React from 'react'
 
-import "./App.css";
+import './App.css'
 
 class App extends React.Component {
-  constructor() {
-    super()
+	constructor() {
+		super()
 
-    this.state = {
-      totalNum: 0
-      
+		this.state = {
+			score: 0,
+			value: 1,
+		}
+	}
+
+	handleClick = () => {
+		this.setState({ score: this.state.score + this.state.value })
+	}
+
+	payPoints = () => {
+		if (this.state.score < 10) {
+			alert("You can't afford that!")
+		} else {
+			this.setState({
+				score: this.state.score - 10,
+				value: this.state.value + 1,
+			})
+		}
+	}
+
+	youWin = () => {
+    this.setState({
+      score: 0,
+      value: 1
+    })
+  }
+
+	render() {
+		if (this.state.score < 100) {
+			return (
+				<div>
+					<h2>Current Score: {this.state.score}</h2>
+					<button onClick={this.handleClick}>+{this.state.value}</button>
+					<button onClick={this.payPoints}>
+						Pay 10 points to change from +{this.state.value} to +{this.state.value + 1}
+					</button>
+				</div>
+			)
+		} else {
+      return (
+        <div>
+          <h1>Current Score: {this.state.score}</h1>
+          <h2>You Win!</h2>
+          <button onClick={this.youWin}>Play again?</button>
+        </div>
+      )
     }
-  }
-
-  handleClick = () => {
-    this.setState({ totalNum : this.state.totalNum + 1 })
-  }
-  render() {
-    return (
-      <div>
-        <h2>Current Score: {this.state.totalNum}</h2>
-        <button onClick={this.handleClick}>+1</button>
-        <button>Pay 10 points to change from +1 to +2</button>
-      </div>
-    )
-  }
+	}
 }
 
-export default App;
+export default App
