e559364ab1d5aac353fb4984af59d2f1dfe06750
Miguel Garcia <miguelgarcia@pursuit.org>
2021-05-05 18:20:42 -0400

    complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import Counter from "./Components/counter";
 
 import "./App.css";
 
 class App extends React.Component {
   render() {
-    return "Hello, world!";
+    return (
+      <div>
+        <Counter />
+      </div>
+    );
   }
 }
 
