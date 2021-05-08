61cfd4f889e54c7a37193890946443a59af21e04
Durdona Djalilova <ddjalilova3@gmail.com>
2021-05-08 14:47:18 -0400

    hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Clicker from "./Clicker"
+import ClickerHooks from "./ClickerHooks"
 import "./App.css";
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Clicker />
+        {/* <Clicker /> */}
+        <ClickerHooks />
       </div>
     );
   }
