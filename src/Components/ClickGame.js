0a974c4d0e8591ff9e0a26914ee282d7d1f0bcc5
DamienYule <damienyule@gmail.com>
2021-05-08 15:51:27 -0400

    Hooks

--- src/Components/ClickGame.js
+++ src/Components/ClickGame.js
@@ -1,58 +1,41 @@
-import React from "react";
-class ClickGame extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      score: 0,
-      increment: 1,
-      // index: 0,
-      // classReset: ["reset", "win"]
+import React, { useState } from "react";
 
-
-    };
-  }
-  changeNumber = () => {
-    if (this.state.score >= 10) {
-      this.setState({ score: this.state.score - 10 })
-      this.setState({ increment: this.state.increment + 1 })
+export default function ClickGame() {
+  const [score, setScore] = useState(0);
+  const [increment, setIncrement] = useState(1);
+  const changeNumber = () => {
+    if (score >= 10) {
+      setScore( (prevScore) => prevScore - 10 )
+      setIncrement( (prevIncrement) => prevIncrement + 1 )
     } else {
       window.alert("You can't afford that!")
     }
   }
-  increment = () => {
-    // if over 100
-    // if (this.state.score >= 100) {
-    //   this.setState({ index: this.state.index = 1 })
-    // }
-    // //else 
-    this.setState({ score: this.state.score + this.state.increment })
+  const inc = () => {
+    setScore( (prevScore) => prevScore + increment )
 
   }
-  reset = () => {
+  const reset = () => {
     // this.setState({ index: this.state.index = 0 })
-    this.setState({ score: this.state.score = 0 })
-    this.setState({ increment: this.state.increment = 1 })
+    setScore( (prevScore) => 0 )
+    setIncrement( (prevIncrement) => 1 )
   }
 
-  render() {
-    if (this.state.score < 100) {
-      return (
-        <div>
-          <h1>Current Score: {this.state.score}</h1>
-          <button onClick={this.increment}>+{this.state.increment}</button>
-          <button onClick={this.changeNumber}>Pay 10 points to change from +{this.state.increment} to +{this.state.increment + 1}</button>
-        </div>
-      )
-    }
-    else {
-      return (
-        <div>
-          <h2>You Win!</h2>
-          <button onClick={this.reset}>Play again?</button>
-        </div>
-      )
-    }
+  if (score < 100) {
+    return (
+      <div>
+        <h1>Current Score: {score}</h1>
+        <button onClick={inc}>+{increment}</button>
+        <button onClick={changeNumber}>Pay 10 points to change from +{increment} to +{increment + 1}</button>
+      </div>
+    )
+  }
+  else {
+    return (
+      <div>
+        <h2>You Win!</h2>
+        <button onClick={reset}>Play again?</button>
+      </div>
+    )
   }
 }
-
-export default ClickGame;
