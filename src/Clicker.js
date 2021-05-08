f1d9eece537e1fdc497a0bc6fd746a22d53b7e80
anasnahil <nahil.anas1@gmail.com>
2021-05-08 15:07:37 -0400

    changed it to hooks

--- src/Clicker.js
+++ src/Clicker.js
@@ -1,47 +1,40 @@
-import React from 'react';
+import { useState }  from 'react';
 
-class Clicker extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {points: 0,
-                    pointsButton: 1,
-                    paymentButton: 2
-                }
-    }
-    playAgain = () => {
-        this.setState({
-            points: 0,
-            pointsButton: 1,
-            payButton: 2,
-        });
+const Clicker = () => {
+    const [points, setPoints] = useState(0)
+    const [pointsButton, setPointsButton] = useState(1)
+    const [paymentButton, setPaymentButton] = useState(2)
+    
+    
+    const playAgain = () => {
+        setPoints(0)
+        setPaymentButton(2)
+        setPointsButton(1)
     }
 
-    encrement = () => {
+    const encrement = () => {
         // const { points } = this.state
-        this.setState({points: this.state.points + this.state.pointsButton })
+        setPoints((prevPoints) => prevPoints + pointsButton)
     }
 
-    pointsPayment = () => {
-        if(this.state.points >= 10){
-            this.setState({
-                points: this.state.points - 10,
-                pointsButton: this.state.pointsButton + 1,
-                paymentButton: this.state.paymentButton + 1,
-            });
+    const pointsPayment = () => {
+        if(points >= 10){
+            setPoints((prevPoints) => prevPoints - 10)
+            setPointsButton((prevPointsButton) => prevPointsButton + 2)
+            setPaymentButton((prevPaymentButton) => prevPaymentButton + 1)
+            
         } else {
-            alert('You can\'t afford that!')
+            alert(`You can't afford that!`)
         }
     };
 
-    render() {
-        const { points, pointsButton, paymentButton } = this.state
         if(points < 100) {
             return (
                 <div>
                     <h1>Current Score: {points}</h1>
-                    <button onClick={this.encrement}>+{pointsButton}</button>
+                    <button onClick={encrement}>+{pointsButton}</button>
                     <p>
-                    <button onClick={this.pointsPayment}>Pay 10 points to change from +{pointsButton} to +{paymentButton}</button>
+                    <button onClick={pointsPayment}>Pay 10 points to change from +{pointsButton} to +{paymentButton}</button>
                     </p>
                 </div>
                     )
@@ -52,12 +45,12 @@ class Clicker extends React.Component {
                     <h1>Current Score: {points}</h1>
                     <h2>You Win!</h2>
                     <p>
-                        <button onClick={this.playAgain}>Play again?</button>
+                        <button onClick={playAgain}>Play again?</button>
                     </p>
                 </div>
             )
         }
     }
-}
+
 
 export default Clicker
