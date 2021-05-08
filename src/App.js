c485527f5f0bb089a8dd94747ee2f065441a347c
Shoaib Dar <SDAR30@gmail.com>
2021-05-08 14:36:12 -0400

    updated using hooks

--- src/App.js
+++ src/App.js
@@ -1,48 +1,92 @@
-import React from "react";
-
+import React, {useState} from "react";
 import "./App.css";
 
-class App extends React.Component {
-  state = { score: 0, incrementValue: 1 }
+const App =()=>{
+  const [score,setScore]= useState(0);
+  const [power,setPower] = useState(1);
 
-  increment = () => {
-    const { score, incrementValue } = this.state;
-    this.setState({ score: score + incrementValue })
+  const increment = () => {
+    setScore(score + power);
+    //this.setState({ score: score + incrementValue })
   }
 
-  increaseValue = () => {
-    const { score, incrementValue } = this.state;
+  const increaseValue = () => {
     if (score >= 10) {
-      this.setState({
-        score: score - 10, incrementValue: incrementValue + 1
-      })
+      setScore(score-10);
+      setPower((prevCount) => prevCount + 1)
     } else {
       alert("You can't afford that!")
     }
   }
 
-  resetGame = () => {
-    const { score, incrementValue } = this.state;
-    this.setState({ score: 0, incrementValue: 1 })
+  const resetGame = () => {
+    setScore(0);
+      setPower(1);
   }
 
-  render() {
-    const { score, incrementValue } = this.state;
     if (score < 100) {
       return (<>
         <h1>Current Score: {score}</h1>
-        <button onClick={this.increment}>+{incrementValue}</button>
+        <button onClick={increment}>+{power}</button>
         <br></br>
         <br></br>
-        <button onClick={this.increaseValue}>Pay 10 points to change from +{incrementValue} to +{incrementValue + 1}</button>
+        <button onClick={increaseValue}>Pay 10 points to change from +{power} to +{power + 1}</button>
       </>)
     } else {
       return (<>
        <h1>Current Score: {score}</h1>
         <h2>You Win!</h2>
-        <button onClick={this.resetGame}>Play again?</button>
+        <button onClick={resetGame}>Play again?</button>
       </>)
     }
-  }
 }
 export default App;
+
+
+// import React from "react";
+// import "./App.css";
+
+// class App extends React.Component {
+//   state = { score: 0, incrementValue: 1 }
+
+//   increment = () => {
+//     const { score, incrementValue } = this.state;
+//     this.setState({ score: score + incrementValue })
+//   }
+
+//   increaseValue = () => {
+//     const { score, incrementValue } = this.state;
+//     if (score >= 10) {
+//       this.setState({
+//         score: score - 10, incrementValue: incrementValue + 1
+//       })
+//     } else {
+//       alert("You can't afford that!")
+//     }
+//   }
+
+//   resetGame = () => {
+//     const { score, incrementValue } = this.state;
+//     this.setState({ score: 0, incrementValue: 1 })
+//   }
+
+//   render() {
+//     const { score, incrementValue } = this.state;
+//     if (score < 100) {
+//       return (<>
+//         <h1>Current Score: {score}</h1>
+//         <button onClick={this.increment}>+{incrementValue}</button>
+//         <br></br>
+//         <br></br>
+//         <button onClick={this.increaseValue}>Pay 10 points to change from +{incrementValue} to +{incrementValue + 1}</button>
+//       </>)
+//     } else {
+//       return (<>
+//        <h1>Current Score: {score}</h1>
+//         <h2>You Win!</h2>
+//         <button onClick={this.resetGame}>Play again?</button>
+//       </>)
+//     }
+//   }
+// }
+// export default App;
