82175ddf30b2eafa04084d10775c8db5968160f8
Sarah Weinstein <sbweins@gmail.com>
2021-05-17 23:28:24 -0400

    Lab Update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,70 @@
 import React from "react";
-
 import "./App.css";
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      score: 0,
+      increment: 1,
+    };
+  }
+
+  handleClick = () => {
+    this.setState({
+      score: this.state.score + this.state.increment,
+    });
+  };
+
+  alert = () => {
+    if (this.state.score < 10) {
+      alert("You can't afford that!");
+    } else {
+      this.setState({
+        score: this.state.score - 10,
+        increment: this.state.increment + 1,
+      });
+    }
+  };
+
+  restartGame = () => {
+    this.setState({
+      score: 0,
+      increment: 1
+    });
+  };
+
   render() {
-    return "Hello, world!";
+    const { score, increment } = this.state;
+
+    if (this.state.score < 100) {
+      return (
+        <div className="clicker-game">
+            <h1>
+              Current Score: {score}
+            </h1>
+            <button onClick={this.handleClick}>+{increment}</button>
+            <br></br>
+            <br></br>
+            <button onClick={this.alert}>
+              Pay 10 points to change from +{this.state.increment} to +
+              {this.state.increment + 1}
+            </button>
+        </div>
+      );
+    } else {
+      return (
+        <div className="clicker-game">
+          <h1>
+            Current Score: {score}
+          </h1>
+
+          <h2>You Win!</h2>
+          <button onClick={this.restartGame}>Play again?</button>
+        </div>
+      );
+    }
   }
 }
 
