4a8e25c92325b8fadf611a6e8c9e46c795b644b2
Obaid Rustemi <orustemi@gmail.com>
2021-05-28 10:47:18 -0400

    convert lab to using hooks

--- src/Score.js
+++ src/Score.js
@@ -1,51 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
-class Score extends React.Component {
-  state = { button1Value: 1, count: 0 };
-  incrementCountBy1 = () => {
-    const { count } = this.state;
-    const { button1Value } = this.state;
-    this.setState({ count: count + button1Value });
+const Score = () => {
+  const [button1Value, setButton1Value] = useState(1);
+  const [count, setCount] = useState(0);
+
+  const incrementCountBy1 = () => {
+    setCount(count + button1Value);
   };
-  incrementButtonBy1 = () => {
-    const { count } = this.state;
-    const { button1Value } = this.state;
+  const incrementButtonBy1 = () => {
     if (count < 10) {
       alert("You can't afford that!");
     } else {
-      this.setState({ button1Value: button1Value + 1 });
-      this.setState({ count: count - 10 });
+      setButton1Value(button1Value + 1);
+      setCount(count - 10);
     }
   };
 
-  reset = () => {
-      this.setState({count: 0})
-      this.setState({button1Value:1})
-  }
-  
-  render() {
-    const { count } = this.state;
-    const { button1Value } = this.state;
-    if (count >= 100) {
-      return (
-        <div>
-          <h2>You Win!</h2>
-          <button onClick={this.reset}>Play again?</button>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <h1>Current Score: {count}</h1>
-          <button onClick={this.incrementCountBy1}>+{button1Value}</button>
-          <button onClick={this.incrementButtonBy1}>
-            Pay 10 points to change from +{button1Value} to +{button1Value + 1}
-          </button>
-        </div>
-      );
-      }
-    }
-  }
+  const reset = () => {
+    setCount(0);
+    setButton1Value(1);
+  };
 
+  if (count >= 100) {
+    return (
+      <div>
+        <h2>You Win!</h2>
+        <button onClick={reset}>Play again?</button>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>Current Score: {count}</h1>
+        <button onClick={incrementCountBy1}>+{button1Value}</button>
+        <button onClick={incrementButtonBy1}>
+          Pay 10 points to change from +{button1Value} to +{button1Value + 1}
+        </button>
+      </div>
+    );
+  }
+};
 
 export default Score;
